3822c27b31bb57a2a2d4b9b873e361af5a6df69e
Ivan <ivon.klass@yandex.ru>
2021-11-27 21:37:49 +0300

    test fix

--- main/tests.py
+++ main/tests.py
@@ -3,5 +3,8 @@ from django.test import TestCase
 
 
 class TestClass(TestCase):
+    """Main class for testing"""
+
     def test_something_that_will_pass(self):
-        self.assertTrue(True)
+        """Test that always will be true"""
+        self.assertTrue(1 + 1 == 2)
