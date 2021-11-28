96753856846162aaa3281316634d82603a9156b0
Ivan <ivon.klass@yandex.ru>
2021-11-28 15:35:48 +0300

    python dotenv

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,9 @@
 """Module for creating tests"""
+import os
 from django.test import TestCase
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class TestClass(TestCase):
@@ -7,4 +11,5 @@ class TestClass(TestCase):
 
     def test_something_that_will_pass(self):
         """Test that always will be true"""
+        print(os.environ['TEST'])
         self.assertTrue(1 + 1 == 2)
